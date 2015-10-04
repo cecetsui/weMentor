937bfa49ac3dddea9ab4ed7f0e4df0369b8d1c70
WHACK <jesscherayilwhack@Jessicas-MacBook-Air.local>
2015-10-04 09:56:44 -0400

    everythign

--- index.js
+++ index.js
@@ -7,29 +7,30 @@ var DATABASE_URL = "postgres://mslskdxuhyrwsc:eRZ5hE5yMQBmBaB89w1VjzgfJK@ec2-75-
 
 var pg = require('pg');
 
-pg.connect(DATABASE_URL, function (err, client, done) {
-	if (err) {
-		return console.error('error fetching client from pool', err);
-	}
-	// client.query('INSERT into user_table (firstName, lastName, headline) VALUES($1, $2, $3) RETURNING id', 
- //            ['Cece', 'Tsui', 'yo'], function (err, result) {
-	// 	done();
-	// 	if (err) {
-	// 		return console.error('error running query', err);
-	// 	}
-	// 	console.log("boom");
-	// 	console.log(result);
-	// // });
+// pg.connect(DATABASE_URL, function (err, client, done) {
+// 	if (err) {
+// 		return console.error('error fetching client from pool', err);
+// 	}
 
-	client.query('SELECT firstName FROM user_table', function (err, results) {
-		done();
-		if (err) {
-			return console.error('error running query', err);
-		}
-		console.log("boom");
-		console.log(results.rows[0]);
-	});
-});
+// 	client.query('INSERT into user_table (firstName, lastName, headline) VALUES($1, $2, $3) RETURNING id', 
+//             ['Cece', 'Tsui', 'yo'], function (err, result) {
+// 		done();
+// 		if (err) {
+// 			return console.error('error running query', err);
+// 		}
+// 		console.log("boom");
+// 		console.log(result);
+// 	 });
+
+// 	client.query('SELECT id FROM user_table', function (err, results) {
+// 		done();
+// 		if (err) {
+// 			return console.error('error running query', err);
+// 		}
+// 		console.log("boom");
+// 		console.log(results.rows[0]);
+// 	});
+// });
 
 
 app.set('port', (process.env.PORT || 5000));
@@ -44,6 +45,46 @@ app.set('view engine', 'ejs');
 
 
 app.get('/', function(request, response) {
+		pg.connect(DATABASE_URL, function (err, client, done) {
+		if (err) {
+			return console.error('error fetching client from pool', err);
+		}
+		var query = client.query('SELECT * FROM user_table');
+		console.log(query);
+		query.on("row", function(row, result){
+			result.addRow(row);
+		})
+		query.on("end", function(result){
+			for (var i = 0; i < result.rows; i++) {
+				console.log('yo');
+				console.log(result.rows[i]);
+			}
+			// console.log(result.rows);
+		});
+		// client.query('select * from user_table ', 
+		//             [request.body.latitude, request.body.longitude], function (err, result) {
+		// 		done();
+		// 		if (err) {
+		// 			return console.error('error running query', err);
+		// 		}
+		// 		console.log("boom");
+		// 		console.log(result);
+		// 	 });
+		// });
+
+		// client.query('INSERT into user_table (latitude, longitude) VALUES($1, $2) RETURNING id', 
+		//             [request.body.latitude, request.body.longitude], function (err, result) {
+		// 		done();
+		// 		if (err) {
+		// 			return console.error('error running query', err);
+		// 		}
+		// 		console.log("boom");
+		// 		console.log(result);
+		// 	 });
+		// });
+
+		});
+
   response.render('pages/index');
 });
 
@@ -83,9 +124,65 @@ app.post('/positiondata', function(request, response){
 	console.log("success");
 	console.log(request.body);
 
+
+	// pg.connect(DATABASE_URL, function (err, client, done) {
+	// 	if (err) {
+	// 		return console.error('error fetching client from pool', err);
+	// 	}
+	// 	var query = client.query('select * from user_table');
+	// 	console.log(query);
+	// 	// client.query('select * from user_table ', 
+	// 	//             [request.body.latitude, request.body.longitude], function (err, result) {
+	// 	// 		done();
+	// 	// 		if (err) {
+	// 	// 			return console.error('error running query', err);
+	// 	// 		}
+	// 	// 		console.log("boom");
+	// 	// 		console.log(result);
+	// 	// 	 });
+	// 	// });
+
+	// 	// client.query('INSERT into user_table (latitude, longitude) VALUES($1, $2) RETURNING id', 
+	// 	//             [request.body.latitude, request.body.longitude], function (err, result) {
+	// 	// 		done();
+	// 	// 		if (err) {
+	// 	// 			return console.error('error running query', err);
+	// 	// 		}
+	// 	// 		console.log("boom");
+	// 	// 		console.log(result);
+	// 	// 	 });
+	// 	// });
+
+	// 	});
+
 });
 
 app.post('/userdata', function(request, response) {
+
+	// client.query('SELECT id FROM user_table', function (err, results) {
+	// 	done();
+	// 	if (err) {
+	// 		return console.error('error running query', err);
+	// 	}
+	// 	console.log("boom");
+	// 	console.log(results.rows[0]);
+	// });
+	// pg.connect(DATABASE_URL, function (err, client, done) {
+	// 	if (err) {
+	// 		return console.error('error fetching client from pool', err);
+	// 	}
+
+	// 	\client.query('INSERT into user_table (id, firstName, lastName, headline, photo) VALUES($1, $2, $3, $4, $5) RETURNING id', 
+	//             [request.body.id, request.body.firstName, request.body.lastName, request.body.headline, request.body.photo], function (err, result) {
+	// 		done();
+	// 		if (err) {
+	// 			return console.error('error running query', err);
+	// 		}
+	// 		console.log("boom");
+	// 		console.log(result);
+	// 		console.log(results.rows[0]);
+	// 	 });
+	// });
 	console.log(request.body.id);
 	console.log(request.body.firstName);
 	console.log(request.body.lastName);
